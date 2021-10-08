0313e6567baff9b6df6abe1e624228df0f4fcb84
artysav97 <56535497+artysav97@users.noreply.github.com>
2021-10-08 18:57:08 +0300

    Delete Task3 directory

--- Task3/prepare_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-data = pd.read_csv('AB_NYC_2019.csv')
-print('no NANS', np.mean(data[data['price'].notna()]['price']))
-data = data['price']
-print('AVG value:', np.mean(data))
-print('VAR value:', np.var(data))
-data.to_csv('AB_NYC_2019_price.csv', index=False, header=False)
--- Task3/run.sh
+++
@@ -1,7 +0,0 @@
-mapred streaming \
-    -file /mapper.py \
-    -file /reducer.py \
-    -mapper mapper.py \
-    -reducer reducer.py \
-    -input /hw/AB_NYC_2019_price.csv \
-    -output $1
